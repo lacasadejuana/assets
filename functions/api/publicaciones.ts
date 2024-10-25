8f443b88bdfddd4d28dd98110dcf58a3707b54ed
Felipe Figueroa <amenadiel@gmail.com>
2024-10-25 17:37:55 -0300

    remove cache all from publicaciones

--- functions/api/publicaciones.ts
+++ functions/api/publicaciones.ts
@@ -17,13 +17,6 @@ export const onRequest: PagesFunction = async ({ next }) => {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
-		},
-		cf: {
-			// Always cache this fetch regardless of content type
-			// for a max of 5 seconds before revalidating the resource
-			cacheTtl: 120,
-			cacheEverything: true,
-			//Enterprise only feature, see Cache API for other plans
-		},
+		}
 	})
 }
